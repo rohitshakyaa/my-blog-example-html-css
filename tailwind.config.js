a78cb1670bbcd14f1fb8ba64440e5a541d353368
Rohit Shakya <rohitshakya@vianet.com.np>
2025-01-11 19:48:36 +0545

    restructured folder structure, created index page, made dist not in gitignore

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: "media",
-  content: ["./*.{html,js}", "./node_modules/flowbite/**/*.js"],
+  content: ["./html/*.{html,js}", "./*.html", "./node_modules/flowbite/**/*.js"],
   theme: {
     extend: {
       colors: {
